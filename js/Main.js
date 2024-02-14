724609011aa47c670cfea8533ab4fdca6a7fdb00
ahmed badawi <ahmedbad063@gmail.com>
2024-02-14 19:17:39 +0200

    update the files

--- js/Main.js
+++ js/Main.js
@@ -38,6 +38,29 @@ const blurHeader = () =>{
 }
 window.addEventListener('scroll', blurHeader)
 
+
+/*=============== Team Members ===============*/
+let imgBx = document.querySelectorAll('.imgBx');
+let contentBx = document.querySelectorAll('.contentBx');
+
+for (var i = 0; i < imgBx.length; i++) {
+  imgBx[i].addEventListener('mouseover', function(){
+    for (var i = 0; i < contentBx.length; i++) {
+      contentBx[i].className = 'contentBx';
+    }
+
+    document.getElementById(this.dataset.id).
+    className = 'contentBx active';
+
+    for (var i = 0 ; i < imgBx.length ; i++) {
+      imgBx[i].className = 'imgBx';
+    }
+
+    this.className = 'imgBx active';
+  })
+}
+
+
 /*=============== Email JS ==>> Contact-File ===============*/
 
 const contactForm = document.getElementById('contact-form'),
